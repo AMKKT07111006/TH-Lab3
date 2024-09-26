8d675370764fb303407fd048a370b4509fad37b9
SV <SV@PM19-PC10>
2024-09-26 15:03:43 +0700

    Add project files.

---
+++ TH-Lab3/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TH_Lab3.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Trang Welcome page viết dưới dạng js inline, sử dụng hộp thoại Prompt
+        // và document.write để nhập liếu
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome2()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
